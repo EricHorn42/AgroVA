6b6df3362dbcc139eb39bae3a817dfc3a5ae677f
Eric Horn <pedrohorn2002@gmail.com>
2025-03-12 16:05:15 -0300

    (feat): Create Initial UI

---
+++ AgroVA.WebUI/Controllers/AnnotationsController.cs
@@ -0,0 +1,24 @@
+﻿using AgroVA.Application.DTOs;
+using AgroVA.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroVA.WebUI.Controllers
+{
+    public class AnnotationsController : Controller
+    {
+        private readonly IAnnotationService _service;
+
+        public AnnotationsController(IAnnotationService service) 
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var anotations = await _service.GetAllAsync();
+            return View(anotations);
+        }
+
+    }
+}
